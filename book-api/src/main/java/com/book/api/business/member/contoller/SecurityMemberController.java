e1631bd7b395aac24284b3153b640c8136378b75
JW0ng <32593055@qq.com>
2018-12-20 17:44:29 +0800

    init framework

---
+++ book-api/src/main/java/com/book/api/business/member/contoller/SecurityMemberController.java
@@ -0,0 +1,17 @@
+package com.book.api.business.member.contoller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 会员安全路由
+ * @Author J.W
+ * @Date 2018/12/20 15:15
+ **/
+@Slf4j
+@RestController
+@RequestMapping("s/m")
+public class SecurityMemberController {
+
+}
