a77114458673c06b10047ec8d0a812cb4e7f6ee3
JW0ng <32593055@qq.com>
2018-12-20 11:23:26 +0800

    init framework

---
+++ book-api/src/main/java/com/book/api/ApiBootstrap.java
@@ -0,0 +1,18 @@
+package com.book.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description api服务
+ * @Author J·W
+ * @Date 2018/12/20 10:09
+ **/
+@SpringBootApplication
+public class ApiBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBootstrap.class, args);
+    }
+
+}
